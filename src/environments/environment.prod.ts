5a144d2bef4ea36b47fcc987650c18b59d60cce9
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-09-05 06:46:25 +0100

    added supabase for backend system

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  supabase: {
+    url: 'https://mbxhabdkusgiyufboasp.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ieGhhYmRrdXNnaXl1ZmJvYXNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5NzA1MTEsImV4cCI6MjA3MjU0NjUxMX0.V76BXqYYNt5IGNCuvaCz-h3nX2fiWLsza6dhdV_lLtk'
+  }
+};
